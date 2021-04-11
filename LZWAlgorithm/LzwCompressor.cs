4a262a11d20628e73ea3b591ce3a275fdacc5890
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-11 23:52:00 +0400

    encode method implemented

--- LZWAlgorithm/LzwCompressor.cs
+++ LZWAlgorithm/LzwCompressor.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LZWAlgorithm
 {
     public class LzwCompressor : IOperations
     {
         public string Data { get; set; }
+
+        private Dictionary<string, int> _dict;
+        private List<int> _encodedList;
 
         public LzwCompressor(string data)
         {
@@ -14,22 +18,44 @@ namespace LZWAlgorithm
 
         public string Encode()
         {
-            var dict = new Dictionary<char, int>();
+            _dict = new Dictionary<string, int>();
             int count = 0;
+            MakeDictionary(_dict, ref count);
+
+            string s = "";
+            _encodedList = new List<int>();
             for (int i = 0; i < Data.Length; i++)
             {
-                if (Data[i] == ' ' || dict.ContainsKey(Data[i])) continue;
-                dict[Data[i]] = count;
-                count++;
-            }
+                if (_dict.ContainsKey(s + Data[i]))
+                {
+                    s += Data[i];
+                    continue;
+                }
 
-            foreach (var dictKey in dict.Keys)
-            {
-                Console.WriteLine(dictKey);
+                _encodedList.Add(_dict[s]);
+                _dict[s + Data[i]] = count;
+                count++;
+                s = "" + Data[i];
             }
 
             string result = "";
+
+            for (int i = 0; i < _encodedList.Count; i++)
+            {
+                result += _encodedList[i];
+            }
+
             return result;
+        }
+
+        private void MakeDictionary(Dictionary<string, int> dict, ref int count)
+        {
+            for (int i = 0; i < Data.Length; i++)
+            {
+                if (Data[i] == ' ' || dict.ContainsKey(Data[i].ToString())) continue;
+                dict[Data[i].ToString()] = count;
+                count++;
+            }
         }
 
         public string Decode()
