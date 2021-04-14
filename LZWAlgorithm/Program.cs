caea85f6ebeb7791868f1fa93f726ba5724ff579
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-14 23:39:00 +0400

    Update Program.cs

--- LZWAlgorithm/Program.cs
+++ LZWAlgorithm/Program.cs
@@ -6,17 +6,17 @@ namespace LZWAlgorithm
     {
         static void Main(string[] args)
         {
-            var lzwCompressor = new LzwCompressor("banana_bandana_bandana_bananabanana");
+            var lzwCompressor = new LzwCompressor("banana_bandana_ban ban bananaana");
             var encodedStr = lzwCompressor.Encode();
             Console.WriteLine($"Encoded data -> %{encodedStr}% ({encodedStr.Length} bytes)");
 
             var decodedStr = lzwCompressor.Decode();
             Console.WriteLine($"Decoded data -> %{decodedStr}%");
 
-            string input = Console.ReadLine();
-            string encData = LzwStaticCompressor.Encode(input);
-            Console.WriteLine($"Encoded data -> %{encData}%");
-            Console.WriteLine($"Decoded data -> %{LzwStaticCompressor.Decode(encData)}%");
+            //string input = Console.ReadLine();
+            //string encData = LzwStaticCompressor.Encode(input);
+            //Console.WriteLine($"Encoded data -> %{encData}%");
+            //Console.WriteLine($"Decoded data -> %{LzwStaticCompressor.Decode(encData)}%");
         }
     }
 }
